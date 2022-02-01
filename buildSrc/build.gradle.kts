b46c56230ef90e694623bf63fb42f8bfaac34ba5
Messias Junior <messiaslima.03@gmail.com>
2022-02-01 14:25:55 +0000

    chore: create core ui module

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,9 +6,11 @@ plugins {
 repositories {
     mavenCentral()
     gradlePluginPortal()
+    google()
 }
 
 dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.41.0")
     implementation("com.star-zero.gradle:githook:1.2.1")
+    //implementation("com.android.tools.build:gradle:7.1.0")
 }
---
+++ buildSrc/src/main/kotlin/plugin/common/AndroidConfig.kt
@@ -0,0 +1,14 @@
+package plugin.common
+
+// import org.gradle.api.Project
+// ////import com.android.build.gradle.LibraryExtension
+// //
+// ////val Project.android: LibraryExtension
+// ////    get() = extensions.findByName("android") as? LibraryExtension
+// ////        ?: error("android plugin not found")
+// //
+// //fun Project.applyAndroidConfig() {
+// //    android.run {
+// //
+// //    }
+// //}
