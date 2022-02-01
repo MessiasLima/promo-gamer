349c691cf720a8309dd8325a3d33484373b02247
Messias Junior <messiaslima.03@gmail.com>
2022-02-01 08:27:34 +0000

    build: update dependencies custom plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,11 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
     id(Android.GradlePlugin.application) version Android.GradlePlugin.version apply false
     id(Android.GradlePlugin.library) version Android.GradlePlugin.version apply false
     id(Kotlin.GradlePlugin.android) version Kotlin.GradlePlugin.androidVersion apply false
     id(Detekt.gradlePlugin) version Detekt.version
-    id("com.github.ben-manes.versions") version "0.41.0"
+    id("dependency-updater")
 }
 
 tasks.register("clean").configure {
     delete(rootProject.buildDir)
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version) || isOnIgnoreList(candidate.module)
-    }
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-fun isOnIgnoreList(module: String): Boolean {
-    return listOf(
-        "jacoco",
-        "android.gradle.plugin"
-    ).any {
-        module.contains(it, ignoreCase = true)
-    }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     `kotlin-dsl`
+    `kotlin-dsl-precompiled-script-plugins`
 }
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.41.0")
 }
