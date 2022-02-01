153ae1dcd0c4baa794b8d4ab8db117c1d56a3459
Messias Junior <messiaslima.03@gmail.com>
2022-02-01 08:54:59 +0000

    style: fix lint issues

--- app/src/main/java/com/messiaslima/promogamer/ui/theme/Theme.kt
+++ app/src/main/java/com/messiaslima/promogamer/ui/theme/Theme.kt
@@ -5,6 +5,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.messiaslima.promogamer.ui.theme.PromoGamerColors.Purple200
+import com.messiaslima.promogamer.ui.theme.PromoGamerColors.Purple500
+import com.messiaslima.promogamer.ui.theme.PromoGamerColors.Purple700
+import com.messiaslima.promogamer.ui.theme.PromoGamerColors.Teal200
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
--- app/src/test/java/com/messiaslima/promogamer/ExampleUnitTest.kt
+++ app/src/test/java/com/messiaslima/promogamer/ExampleUnitTest.kt
@@ -1,6 +1,6 @@
 package com.messiaslima.promogamer
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
