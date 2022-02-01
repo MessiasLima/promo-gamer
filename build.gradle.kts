fc75e4b0c084d65ae8655860c2a4434f319eae73
Messias Junior <messiaslima.03@gmail.com>
2022-02-01 17:19:48 +0000

    chore: refactor dependency updater plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,13 @@ plugins {
     id(Android.GradlePlugin.application) version Android.GradlePlugin.version apply false
     id(Android.GradlePlugin.library) version Android.GradlePlugin.version apply false
     id(Kotlin.GradlePlugin.android) version Kotlin.GradlePlugin.androidVersion apply false
-    id(Detekt.gradlePlugin) version Detekt.version
     id(GitHook.gradlePlugin) version GitHook.version apply false
-    id("dependency-updater")
+    id(DependencyUpdater.gradlePlugin) version DependencyUpdater.version apply false
+    id(Detekt.gradlePlugin) version Detekt.version
 }
 
 apply(from = "$rootDir/script/githook.gradle")
+apply(from = "$rootDir/script/dependency-updater.gradle")
 
 tasks.register("clean").configure {
     delete(rootProject.buildDir)
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,14 +1,7 @@
 plugins {
     `kotlin-dsl`
-    `kotlin-dsl-precompiled-script-plugins`
 }
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
-    google()
-}
-
-dependencies {
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.41.0")
 }
