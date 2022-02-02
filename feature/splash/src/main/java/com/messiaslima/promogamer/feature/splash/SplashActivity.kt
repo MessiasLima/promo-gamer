339ce9c022b96cb04c1ce3439ee6bb308db66a8c
Messias Junior <messiaslima.03@gmail.com>
2022-02-02 09:27:39 +0000

    feat: create splash screen theme

--- feature/splash/src/main/java/com/messiaslima/promogamer/feature/splash/SplashActivity.kt
+++ feature/splash/src/main/java/com/messiaslima/promogamer/feature/splash/SplashActivity.kt
@@ -4,12 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.messiaslima.promogamer.core.ui.theme.PromoGamerTheme
 
@@ -19,13 +15,7 @@ class SplashActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PromoGamerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Splash screen")
-                }
+                Greeting("Splash screen")
             }
         }
     }
