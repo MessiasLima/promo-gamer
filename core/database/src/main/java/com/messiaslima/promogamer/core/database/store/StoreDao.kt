f6b05dac2e012c6f3f9102f43dd1ab4731a55433
Messias Junior <messiaslima.03@gmail.com>
2022-02-08 09:13:32 +0000

    feat: create database configuration

---
+++ core/database/src/main/java/com/messiaslima/promogamer/core/database/store/StoreDao.kt
@@ -0,0 +1,17 @@
+package com.messiaslima.promogamer.core.database.store
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface StoreDao {
+    @Insert
+    fun save(vararg users: StoreEntity)
+
+    @Query("SELECT * FROM store")
+    fun findAll(): List<StoreEntity>
+
+    @Query("SELECT * FROM store WHERE id = :id")
+    fun findById(id: Int): StoreEntity
+}
