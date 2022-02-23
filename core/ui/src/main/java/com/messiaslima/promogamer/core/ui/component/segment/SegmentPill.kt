22470a35ad6e2e4d826d0c70efdf0fdab9f8bfef
messiaslima <messiaslima.03@gmail.com>
2022-02-23 08:51:20 +0000

    fix: segment pill clip

--- core/ui/src/main/java/com/messiaslima/promogamer/core/ui/component/segment/SegmentPill.kt
+++ core/ui/src/main/java/com/messiaslima/promogamer/core/ui/component/segment/SegmentPill.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,6 +37,7 @@ fun SegmentPill(
 
     Box(
         modifier = modifier
+            .clip(roundedCorner)
             .background(color = backgroundColor, shape = roundedCorner)
             .clickable { onClick() }
             .padding(4.dp)
