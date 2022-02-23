0141f60c23d69b52458cc40a7fbf520c538fc2de
messiaslima <messiaslima.03@gmail.com>
2022-02-23 07:58:39 +0000

    fix: screen alignment

--- feature/home/src/main/java/com/messiaslima/promogamer/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/messiaslima/promogamer/feature/home/HomeScreen.kt
@@ -1,12 +1,16 @@
 package com.messiaslima.promogamer.feature.home
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.messiaslima.promogamer.core.navigation.contract.AppNavigator
@@ -25,12 +29,14 @@ fun HomeScreen(
         val (navHostWrapper, bottomNavigation) = createRefs()
 
         Box(
-            modifier = Modifier.constrainAs(navHostWrapper) {
-                top.linkTo(parent.top)
-                bottom.linkTo(bottomNavigation.top)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            },
+            modifier = Modifier
+                .constrainAs(navHostWrapper) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(bottomNavigation.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    height = Dimension.fillToConstraints
+                },
         ) {
             if (mainNavController != null && compositeNavigators != null) {
                 HomeNavHost(
@@ -44,6 +50,7 @@ fun HomeScreen(
         AppBottomNavigation(
             modifier = Modifier.constrainAs(bottomNavigation) {
                 bottom.linkTo(parent.bottom)
+                top.linkTo(navHostWrapper.bottom)
             },
             compositeNavigators = compositeNavigators,
             homeNavController = homeNavController
